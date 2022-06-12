4d42b7156c9695e9aff280cc9eb9e66834015340
Samaresh Samanta <ss9261@srmist.edu.in>
2022-06-12 16:39:01 +0530

    add: task card frontend dashBoard

--- frontend/src/components/dashboard/dashboard.comonent.js
+++ frontend/src/components/dashboard/dashboard.comonent.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import styles from './dashboard.module.css'
 import Logo from "../../assets/logo.svg"
+import { AiFillEdit } from 'react-icons/ai'
+import { MdMail } from 'react-icons/md'
+import { SiGooglemeet } from 'react-icons/si'
+import { AiFillCheckSquare } from 'react-icons/ai'
 
 function DashboardComponent() {
     return (
@@ -108,6 +112,202 @@ function DashboardComponent() {
                         <div className={styles.account}>
                             <div className={styles.miniAvatar}></div>
                             Samaresh Sama..
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.taskHolder}>
+                    <div className={styles.card}>
+                        <div className={styles.headerCtn}>
+                            <h3>Frontend Develop</h3>
+                            <AiFillEdit />
+                        </div>
+                        <div className={styles.detailsCtn}>
+                            <p>
+                                Study the design of the website and code it from scratch to full..
+                            </p>
+                        </div>
+                        <div className={styles.actionCtn}>
+                            <AiFillCheckSquare style={{color:'#2684FC'}}/>
+                            <SiGooglemeet style={{color:'blue'}}/>
+                            <MdMail style={{color:'#00AC47'}}/>
+                        </div>
+                        <h5>Members:</h5>
+                        <div className={styles.membersCtn}>
+                            <div>
+                                <p>Samaresh Samanta</p>
+                            </div>
+
+                            <div>
+                                <p>Anubhav Bajpai</p>
+                            </div>
+
+                            <div>
+                                <p>Ankan Kundu</p>
+                            </div>
+
+                            <div>
+                                <p>Ankit</p>
+                            </div>
+
+                            <div>
+                                <p>Sahil</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={styles.card}>
+                        <div className={styles.headerCtn}>
+                            <h3>Frontend Develop</h3>
+                            <AiFillEdit />
+                        </div>
+                        <div className={styles.detailsCtn}>
+                            <p>
+                                Study the design of the website and code it from scratch to full..
+                            </p>
+                        </div>
+                        <div className={styles.actionCtn}>
+                            <AiFillCheckSquare style={{color:'#2684FC'}}/>
+                            <SiGooglemeet style={{color:'blue'}}/>
+                            <MdMail style={{color:'#00AC47'}}/>
+                        </div>
+                        <h5>Members:</h5>
+                        <div className={styles.membersCtn}>
+                            <div>
+                                <p>Samaresh Samanta</p>
+                            </div>
+
+                            <div>
+                                <p>Anubhav Bajpai</p>
+                            </div>
+
+                            <div>
+                                <p>Ankan Kundu</p>
+                            </div>
+
+                            <div>
+                                <p>Ankit</p>
+                            </div>
+
+                            <div>
+                                <p>Sahil</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={styles.card}>
+                        <div className={styles.headerCtn}>
+                            <h3>Frontend Develop</h3>
+                            <AiFillEdit />
+                        </div>
+                        <div className={styles.detailsCtn}>
+                            <p>
+                                Study the design of the website and code it from scratch to full..
+                            </p>
+                        </div>
+                        <div className={styles.actionCtn}>
+                            <AiFillCheckSquare style={{color:'#2684FC'}}/>
+                            <SiGooglemeet style={{color:'blue'}}/>
+                            <MdMail style={{color:'#00AC47'}}/>
+                        </div>
+                        <h5>Members:</h5>
+                        <div className={styles.membersCtn}>
+                            <div>
+                                <p>Samaresh Samanta</p>
+                            </div>
+
+                            <div>
+                                <p>Anubhav Bajpai</p>
+                            </div>
+
+                            <div>
+                                <p>Ankan Kundu</p>
+                            </div>
+
+                            <div>
+                                <p>Ankit</p>
+                            </div>
+
+                            <div>
+                                <p>Sahil</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={styles.card}>
+                        <div className={styles.headerCtn}>
+                            <h3>Frontend Develop</h3>
+                            <AiFillEdit />
+                        </div>
+                        <div className={styles.detailsCtn}>
+                            <p>
+                                Study the design of the website and code it from scratch to full..
+                            </p>
+                        </div>
+                        <div className={styles.actionCtn}>
+                            <AiFillCheckSquare style={{color:'#2684FC'}}/>
+                            <SiGooglemeet style={{color:'blue'}}/>
+                            <MdMail style={{color:'#00AC47'}}/>
+                        </div>
+                        <h5>Members:</h5>
+                        <div className={styles.membersCtn}>
+                            <div>
+                                <p>Samaresh Samanta</p>
+                            </div>
+
+                            <div>
+                                <p>Anubhav Bajpai</p>
+                            </div>
+
+                            <div>
+                                <p>Ankan Kundu</p>
+                            </div>
+
+                            <div>
+                                <p>Ankit</p>
+                            </div>
+
+                            <div>
+                                <p>Sahil</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={styles.card}>
+                        <div className={styles.headerCtn}>
+                            <h3>Frontend Develop</h3>
+                            <AiFillEdit />
+                        </div>
+                        <div className={styles.detailsCtn}>
+                            <p>
+                                Study the design of the website and code it from scratch to full..
+                            </p>
+                        </div>
+                        <div className={styles.actionCtn}>
+                            <AiFillCheckSquare style={{color:'#2684FC'}}/>
+                            <SiGooglemeet style={{color:'blue'}}/>
+                            <MdMail style={{color:'#00AC47'}}/>
+                        </div>
+                        <h5>Members:</h5>
+                        <div className={styles.membersCtn}>
+                            <div>
+                                <p>Samaresh Samanta</p>
+                            </div>
+
+                            <div>
+                                <p>Anubhav Bajpai</p>
+                            </div>
+
+                            <div>
+                                <p>Ankan Kundu</p>
+                            </div>
+
+                            <div>
+                                <p>Ankit</p>
+                            </div>
+
+                            <div>
+                                <p>Sahil</p>
+                            </div>
                         </div>
                     </div>
                 </div>
