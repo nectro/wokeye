4ec761333ee8fe1cbdcbae5e390b509d5c01841e
Samaresh Samanta <ss9261@srmist.edu.in>
2022-06-25 23:43:15 +0530

    add: public api_uri

--- frontend/src/constants/config.constant.js
+++ frontend/src/constants/config.constant.js
@@ -1,1 +1,2 @@
-export const API_URI="http://localhost:5000/";
+export const API_URI="https://wokeyebackend.azurewebsites.net/";  //for production
+// export const API_URI="http://localhost:5000/";  //for development
